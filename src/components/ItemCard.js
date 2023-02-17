170c9843273a58384f83a4dd324f55a712d8b02c
Paul Okoh <pauldokoh@gmail.com>
2023-02-17 16:01:53 -0600

    add skeletal backend server

---
+++ src/components/ItemCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+const ItemCard = () => {
+  return (
+
+    
+    <div>
+      <img src="assets/macbook.jpeg" alt="photo of macbook laptop" />
+      <h3>MacBook Air</h3>
+      <h3>Gently used mac book</h3>
+      <h3>$250.00</h3>
+      <button>Add to cart</button>
+    </div>
+  )
+}
+
+export default ItemCard
